d0f54d933400d6960755db7c1c9cd74db2cf7fe5
Rajiv <1998irajeev@gmail.com>
2021-05-22 14:47:34 +0530

    part 4 completed: added a message service

---
+++ src/app/message.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+  add(message: string) {
+    console.log(message)
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+
+  constructor() {}
+}
